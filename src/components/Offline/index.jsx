50820b046f43386c888ea40eace928ede9fb4625
JanaKoduje <jjanule@gmail.com>
2022-09-29 20:18:55 +0200

    update componenty Offline

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -3,6 +3,30 @@ import React, { useEffect, useState } from 'react';
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
 
+  useEffect(
+    () => {
+        window.addEventListener('offline', handleOffline)
+        window.addEventListener('online', handleOnline)
+        console.log('mounted')
+
+        return () => {
+          console.log('unmounted')
+          window.removeEventListener('offline', handleOffline)
+          window.removeEventListener('online', handleOnline)
+        }
+    }, []
+
+  )
+
+  const handleOffline = () => {
+    setIsOnline(false)
+    console.log('jste offline')
+  }
+
+  const handleOnline = () => {
+    setIsOnline(true)
+  }
+
   if (isOnline) {
     return (
       <p>Všechno v pořádku</p>
